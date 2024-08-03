813eb47f25dc8bc5d09491bbee9fcaaa9ae8e94c
Pavlos Smith <57727226+iByteABit256@users.noreply.github.com>
2024-08-04 01:47:35 +0300

    Utilise mrn-generator crate instead of calling the cli tool (#2)
    * UI now utilizes the mrn-generator crate instead of calling the cli tool
    
    * Made small improvements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use core::f32;
 use eframe::egui;
-use std::process::Command;
+use mrn_generator::{generate_random_mrn, match_procedure};
 
 fn main() -> eframe::Result {
     eframe::run_native(
@@ -66,55 +66,75 @@ impl eframe::App for MrnGeneratorApp {
             if ui.button("Generate MRNs").clicked() {
                 self.output = generate_mrns(
                     &self.country_code,
-                    &self.number_of_mrns,
-                    &self.procedure_category,
-                    &self.combined,
-                    &self.declaration_office,
+                    self.number_of_mrns.parse::<u32>().unwrap_or(1),
+                    (!self.procedure_category.is_empty()).then_some(&self.procedure_category),
+                    (!self.combined.is_empty()).then_some(&self.combined),
+                    (!self.declaration_office.is_empty()).then_some(&self.declaration_office),
                 );
             }
 
             ui.separator();
             ui.label("Output:");
-            ui.text_edit_multiline(&mut self.output);
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.text_edit_multiline(&mut self.output);
+            });
         });
     }
 }
 
 fn generate_mrns(
     country_code: &str,
-    number_of_mrns: &str,
-    procedure_category: &str,
-    combined: &str,
-    declaration_office: &str,
+    number_of_mrns: u32,
+    procedure_category_opt: Option<&str>,
+    combined_opt: Option<&str>,
+    declaration_office_opt: Option<&str>,
 ) -> String {
-    let mut command = Command::new("mrn-generator.exe");
+    let mut output = String::new();
 
-    command.arg("--country-code").arg(country_code);
-
-    if !number_of_mrns.is_empty() {
-        command.arg("--number-of-mrns").arg(number_of_mrns);
+    if country_code.is_empty() {
+        return "Please input a country code with two characters (e.g. 'IT')".to_string();
     }
 
-    if !procedure_category.is_empty() {
-        command.arg("--procedure-category").arg(procedure_category);
-    }
+    for _ in 0..number_of_mrns {
+        let mut is_err = false;
 
-    if !combined.is_empty() {
-        command.arg("--combined").arg(combined);
-    }
+        let mut procedure_opt = None;
+        if let Some(procedure_category) = procedure_category_opt {
+            let procedure_res = match_procedure(procedure_category, combined_opt);
 
-    if !declaration_office.is_empty() {
-        command.arg("--declaration-office").arg(declaration_office);
-    }
+            match procedure_res {
+                Ok(procedure) => procedure_opt = Some(procedure),
+                Err(e) => {
+                    output = e.to_string();
+                    is_err = true;
+                }
+            }
+        } else if let Some(combined) = combined_opt {
+            output = format!("You can't combine {combined} without providing a procedure category")
+                .to_string();
+            break;
+        };
 
-    match command.output() {
-        Ok(output) => {
-            if output.status.success() {
-                String::from_utf8_lossy(&output.stdout).to_string()
-            } else {
-                format!("Error: {}", String::from_utf8_lossy(&output.stderr))
+        if is_err {
+            break;
+        }
+
+        let mrn_res = generate_random_mrn(country_code, procedure_opt, declaration_office_opt);
+
+        match mrn_res {
+            Ok(mrn) => {
+                if output.is_empty() {
+                    output = mrn
+                } else {
+                    output = [output, mrn].join("\n")
+                };
+            }
+            Err(e) => {
+                output = e.to_string();
+                break;
             }
         }
-        Err(e) => format!("Failed to execute process: {}", e),
     }
+
+    output
 }
